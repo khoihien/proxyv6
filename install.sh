65c26b86f9b5c0c9cc03022c5f60de9a561b426c
khoihien <60836857+khoihien@users.noreply.github.com>
2024-08-15 10:49:12 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,16 +12,22 @@ gen64() {
 	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
 install_3proxy() {
-    echo "installing 3proxy"
-    URL="https://raw.githubusercontent.com/quayvlog/quayvlog/main/3proxy-3proxy-0.8.6.tar.gz"
-    wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-3proxy-0.8.6
-    make -f Makefile.Linux
-    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
-    cp src/3proxy /usr/local/etc/3proxy/bin/
-    cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
-    chmod +x /etc/init.d/3proxy
-    chkconfig 3proxy on
+    #!echo "installing 3proxy"
+    #!URL="https://raw.githubusercontent.com/quayvlog/quayvlog/main/3proxy-3proxy-0.8.6.tar.gz"
+    #!wget -qO- $URL | bsdtar -xvf-
+    #!cd 3proxy-3proxy-0.8.6
+    #!make -f Makefile.Linux
+    #!mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
+    #!cp src/3proxy /usr/local/etc/3proxy/bin/
+    #!cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
+    #!chmod +x /etc/init.d/3proxy
+    #!chkconfig 3proxy on
+    #!cd $WORKDIR
+    git clone https://github.com/z3apa3a/3proxy
+    cd 3proxy
+    ln -s Makefile.Linux Makefile
+    make
+    sudo make install
     cd $WORKDIR
 }
 
